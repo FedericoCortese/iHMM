aacad1fea2c3c15dd82cce88c0958a4ff71e6bf0
Federico P. Cortese <67327161+FedericoCortese@users.noreply.github.com>
2025-09-02 15:35:36 +0200

    noth

--- example.R
+++ example.R
@@ -17,8 +17,8 @@ hypers$alpha0_a = 4;
 hypers$alpha0_b = 1;
 hypers$gamma_a = 3;
 hypers$gamma_b = 6;
-hypers$alpha0= 1;
-hypers$gamma= 1;
+# hypers$alpha0= 1;
+# hypers$gamma= 3;
 hypers$sigma2 = 1.5;
 hypers$mu_0 = 0.0;
 hypers$sigma2_0 = 1.0;
@@ -28,4 +28,4 @@ nums=1000
 numi=1
 S0=NULL
 
-fit=iHmmNormalSampleBeam(Y, hypers=hypers, numb=100, nums=1000, numi=5, S0=NULL,K0=5)
+fit=iHmmNormalSampleBeam(Y, hypers=hypers, numb=100, nums=1000, numi=5, S0=NULL,K0=2)
